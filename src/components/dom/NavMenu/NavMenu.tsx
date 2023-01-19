3b66b78ca285b86052e3c413e6042f6608d1c8a0
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-01-19 16:56:06 -0600

    Nav expands and contracts correctly, code is scuffed

--- src/components/dom/NavMenu/NavMenu.tsx
+++ src/components/dom/NavMenu/NavMenu.tsx
@@ -2,16 +2,73 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import classNames from 'classnames'
 import { useReducer } from 'react'
+import { motion, AnimatePresence, Variants } from 'framer-motion'
+import useElementSize from './useElementSize'
 
-const links = Array.from({ length: 8 }, (_, i) => ({
+const links = Array.from({ length: 7 }, (_, i) => ({
   href: `link${i}`,
   text: `Link ${i}`,
   isActive: i === 3 ? true : false,
 }))
 
+const backgroundVariants = {
+  open: (height = 600) => ({
+    width: '200px',
+    height,
+    transition: {
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+      // restDelta: 2,
+    },
+  }),
+  closed: {
+    width: '48px',
+    height: '48px',
+    transition: {
+      delay: 0.8,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+} satisfies Variants
+
+const navVariants = {
+  open: { opacity: 1, transition: { time: 0.25, delay: 0.25 } },
+  closed: { opacity: 0, transition: { time: 0.25, delay: 0.25 } },
+} satisfies Variants
+
+const itemContainerVariants = {
+  open: {
+    transition: { staggerChildren: 0.05, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+} satisfies Variants
+
+const itemVariants = {
+  open: {
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    }
+  }
+} satisfies Variants
+
 function NavMenu() {
   const router = useRouter()
   const [menuOpen, toggleMenuOpen] = useReducer((o) => !o, false)
+  const [navRef, { height }] = useElementSize()
+
+  const bgHeight = height + 48 + 16
 
   const toggleGui = (checked: boolean) => {
     router.push(
@@ -25,53 +82,67 @@ function NavMenu() {
   }
 
   return (
-    <div
-      className={classNames(
-        'absolute z-10 p-1 border shadow-lg shadow-zinc-900 rounded-2xl bg-zinc-800 border-zinc-700 left-4 top-4',
-        menuOpen ? 'w-[200px]' : 'w-[48px] h-[48px]',
-      )}>
-      <button
-        onClick={toggleMenuOpen}
+    <motion.div
+      animate={menuOpen ? 'open' : 'closed'}
+    >
+      <motion.div
+        variants={backgroundVariants}
+        custom={bgHeight}
         className={classNames(
-          'rounded-2xl hover:bg-zinc-700/50 transition-all',
-          menuOpen ? 'p-1' : 'absolute inset-0 text-center',
-        )}>
-        {/* do something nice with the svg animation stuff */}
-        {menuOpen ? <CloseIcon /> : <MenuIcon />}
-      </button>
-      {menuOpen && (
-        <div>
-          <div className='p-3'>
-            <label className='flex items-center justify-between mt-2 text-sm font-semibold cursor-pointer text-zinc-300'>
-              <span>Toggle GUI</span>
-              <input
-                checked={Boolean(router.query.gui)}
-                onChange={(e) => toggleGui(e.target.checked)}
-                type='checkbox'
-                className='text-indigo-700 border-0 rounded cursor-pointer focus:ring-0 focus:ring-offset-1 outline:0 shadow-input bg-zinc-900'
-              />
-            </label>
-          </div>
-          <nav>
-            <ul className='flex flex-col'>
-              {links.map((link, index) => (
-                <Link key={index} href={link.href}>
-                  <li
+          'absolute z-10 border shadow-lg shadow-zinc-900 rounded-2xl bg-zinc-800 border-zinc-700 left-4 top-4',
+          menuOpen ? 'w-[200px]' : 'w-[48px] h-[48px]',
+        )}></motion.div>
+      <button onClick={toggleMenuOpen} className={'absolute z-20 top-4 left-4 p-[11px] w-[48px] h-[48px] rounded-2xl text-zinc-500 grid place-items-center'}>
+        <AnimatePresence>
+          {menuOpen ? (
+            <motion.div className="absolute" key="close" layoutId="menu-button" initial={{ opacity: 0 }} animate={{ opacity: 1, }} exit={{ opacity: 0, transition: { delay: 1 } }}>
+              <CloseIcon />
+            </motion.div>
+          ) : (
+            <motion.div className="absolute" key="open" layoutId="menu-button" initial={{ opacity: 0, }} animate={{ opacity: 1, transition: { delay: 1 } }} exit={{ opacity: 0, }}>
+              <MenuIcon />
+            </motion.div>
+          )
+          }
+        </AnimatePresence >
+      </button >
+      <div
+        ref={navRef}
+        className="absolute z-20 mt-16 left-4 w-[200px]"
+      >
+        <nav>
+          <motion.ul variants={itemContainerVariants} className='flex flex-col'>
+            <motion.li initial={false} key={-1} variants={itemVariants} className='px-4 pb-3'>
+              <label className='flex items-center justify-between mt-2 text-sm font-semibold cursor-pointer text-zinc-300'>
+                <span>Toggle GUI</span>
+                <input
+                  checked={Boolean(router.query.gui)}
+                  onChange={(e) => toggleGui(e.target.checked)}
+                  type='checkbox'
+                  className='text-indigo-700 border-0 cursor-pointer focus:ring-0 focus:ring-offset-1 outline:0 shadow-input bg-zinc-900'
+                />
+              </label>
+            </motion.li>
+            {links.map((link, index) => (
+              <motion.li initial={false} key={index} variants={itemVariants}>
+                <Link href={link.href}>
+                  <div
                     className={classNames(
-                      'relative px-3 py-3 overflow-hidden rounded-lg cursor-pointer transition-colors duration-250 hover:bg-zinc-700',
+                      'relative px-5 py-3 overflow-hidden cursor-pointer transition-colors duration-250 hover:bg-zinc-700',
+                      link.isActive ? 'text-zinc-100' : 'text-zinc-400',
                     )}>
                     {link.isActive && (
-                      <span className='absolute inset-0 border-l-4 bg-indigo-600/10 border-l-indigo-900 z-[-1]' />
+                      <span className='absolute inset-0 border-l-4 border-l-indigo-900 z-[-1] bg-indigo-600/10' />
                     )}
                     {link.text}
-                  </li>
+                  </div>
                 </Link>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      )}
-    </div>
+              </motion.li>
+            ))}
+          </motion.ul>
+        </nav>
+      </div >
+    </motion.div>
   )
 }
 
---
+++ src/components/dom/NavMenu/useElementSize.ts
@@ -0,0 +1,77 @@
+import {
+  useCallback,
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  RefObject,
+} from 'react'
+
+const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect
+
+function useEventListener<
+  K extends keyof HTMLElementEventMap,
+  T extends HTMLElement = HTMLDivElement,
+>(
+  eventName: K,
+  handler: (event: HTMLElementEventMap[K]) => void,
+  element?: RefObject<T>,
+  options?: boolean | AddEventListenerOptions,
+) {
+  const savedHandler = useRef(handler)
+
+  useIsomorphicLayoutEffect(() => {
+    savedHandler.current = handler
+  }, [handler])
+
+  useEffect(() => {
+    const targetElement = element?.current
+    if (!(targetElement && targetElement.addEventListener)) return
+
+    const listener: typeof handler = (event) => savedHandler.current(event)
+
+    targetElement.addEventListener(eventName, listener, options)
+
+    return () => {
+      targetElement.removeEventListener(eventName, listener, options)
+    }
+  }, [eventName, element, options])
+}
+
+export type Size = {
+  width: number
+  height: number
+}
+
+function useElementSize<T extends HTMLElement = HTMLDivElement>(): [
+  (node: T | null) => void,
+  Size,
+] {
+  // Mutable values like 'ref.current' aren't valid dependencies
+  // because mutating them doesn't re-render the component.
+  // Instead, we use a state as a ref to be reactive.
+  const [ref, setRef] = useState<T | null>(null)
+  const [size, setSize] = useState<Size>({
+    width: 0,
+    height: 0,
+  })
+
+  // prevent too many rendering using useCallback
+  const handleSize = useCallback(() => {
+    setSize({
+      width: ref?.offsetWidth || 0,
+      height: ref?.offsetHeight || 0,
+    })
+  }, [ref?.offsetHeight, ref?.offsetWidth])
+
+  useEventListener('resize', handleSize)
+
+  useIsomorphicLayoutEffect(() => {
+    handleSize()
+  }, [ref?.offsetHeight, ref?.offsetWidth])
+
+  return [setRef, size]
+}
+
+export default useElementSize
